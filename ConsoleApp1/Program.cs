131bedfe5d8283eef1aa73a4a0c1e3f0ef4a506b
smz158 <sultan22cs@gmail.com>
2021-04-11 21:24:30 +0300

    first test

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 256;
+            byte b = (byte )a;
+            Console.WriteLine(b);
+        }
+    }
+}
